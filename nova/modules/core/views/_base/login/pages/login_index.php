143ca72b603101d58a16b5feb2ad29092cb1b5be
Jon Matterson <jonm@anodyne-productions.com>
2012-10-07 11:34:27 -0700

    Remove excess br tags from login index [#212]

--- nova/modules/core/views/_base/login/pages/login_index.php
+++ nova/modules/core/views/_base/login/pages/login_index.php
@@ -3,7 +3,7 @@
 <?php echo text_output($header, 'h1', 'page-head');?>
 
 <?php echo form_open('login/check_login');?>
-	<br /><p>
+	<p>
 		<kbd><?php echo $label['email'];?></kbd>
 		<?php echo form_input($inputs['email']);?>
 	</p>
@@ -18,5 +18,5 @@
 		<?php echo form_checkbox($inputs['remember_me']);?>
 	</p>
 	
-	<br /><p><?php echo form_button($button_login);?></p>
+	<p><?php echo form_button($button_login);?></p>
 <?php echo form_close();?>
