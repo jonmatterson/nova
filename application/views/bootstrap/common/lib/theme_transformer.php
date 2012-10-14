55be6a218e3111019f2a6ec90b3bf4e3f745ec11
Jon Matterson <jonm@anodyne-productions.com>
2012-10-13 20:47:17 -0700

    Add main template and main and personnel pages formatted via Bootstrap

---
+++ application/views/bootstrap/common/lib/theme_transformer.php
@@ -0,0 +1,26 @@
+<?php
+
+class Theme_Transformer
+{
+	/*
+	 * Accepts the complete <ul>...</ul> element assigned to the template view
+	 * as $nav_main and attaches the Twitter bootstrap .nav-bar styling to it.
+	 */
+	public static function nav_main($nav_main, $header = 'USS Enigma', $fixed_top = false)
+	{
+		$before = '<div class="navbar"><div class="navbar-inner"><div class="container">
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+					<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+				</a>';
+		if($header)
+			$before .= '<a class="brand" href="'.Theme_URL::index().'">'.$header.'</a>';
+		$before .= '<div class="nav-collapse">';
+		$after = '</div></div></div></div>';
+		return $before.str_replace('<ul', '<ul class="nav"', $nav_main).$after;
+	}
+	
+	public static function nav_sub($nav_sub)
+	{
+		return str_replace('<ul', '<ul class="nav nav-tabs nav-stacked"', $nav_sub);
+	}
+}
