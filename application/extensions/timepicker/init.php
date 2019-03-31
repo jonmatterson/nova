b6529fd1dbaa84913c53d1e6dddea69d767fd310
Jon Matterson <jon.matterson@gmail.com>
2019-03-31 14:26:22 -0700

    Minor updates to timepicker extension

--- application/extensions/timepicker/init.php
+++ application/extensions/timepicker/init.php
@@ -1,20 +1,8 @@
 <?php 
 
 $this->event->listen(['template', 'render', 'data'], function($event){
-  
-  $event['data']['javascript'] .= '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css">
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js"></script>
-<script type="text/javascript">
-function initializeTimepicker(ele){
-  var options = JSON.parse($(ele).attr("data-timepicker"));
-  if(!$.isPlainObject(options)) options = {};
-  $(ele).timepicker(options)
-}
-$(document).ready(function(){
-  $("[data-timepicker]").each(function(){
-    initializeTimepicker(this)
-  })
-});
-</script>';
-  
+  $event['data']['javascript'] 
+        .= '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css">'
+         . '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js"></script>'
+         . $this->extension['timepicker']->inline_js('timepicker', 'global');
 });
